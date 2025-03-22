cafac9d4e2899c923634009ef90a1e1f6fbd48ff
Ali <raju20dh@gmail.com>
2025-03-22 12:45:03 +0600

    action update new

--- cypress/e2e/fund-transfer.spec.ts
+++ cypress/e2e/fund-transfer.spec.ts
@@ -10,12 +10,14 @@ describe("Verify the successful fund transfer", () => {
   });
 
   it("Fund transfer & verify", () => {
-      ft.NaviagationLink().first().click();
-      ft.amount().clear().type('10');
-      ft.fromAccount().select(0);
-      ft.toAccount().select(1);
-      ft.transferButton().click();
-      ft.transferConfirmation().should('contain.text', '$10.00 has been transferred from account #15897 to account #16341.');
+    ft.NaviagationLink().first().click();
+    ft.amount().clear().type("10");
+    ft.fromAccount().select(0);
+    ft.toAccount().select(1);
+    ft.transferButton().click();
+    ft.transferConfirmation().should(
+      "contain.text",
+      "$10.00 has been transferred from account #17895 to account #18006."
+    );
   });
-    
 });
